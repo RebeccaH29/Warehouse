b16c93ec916e30fe2ad7dfcd6cf13f110ab6219b
Rebecca Henderson <montyp92@gmail.com>
2019-04-07 22:19:32 +0100

    initial build

---
+++ precache-manifest.c8ccf901e13c80c889479488ba61e118.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80278509593807a66033",
+    "url": "/Warehouse/static/js/runtime~main.98f74178.js"
+  },
+  {
+    "revision": "696e0facaaaa33823207",
+    "url": "/Warehouse/static/js/main.5481e7ea.chunk.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/Warehouse/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "696e0facaaaa33823207",
+    "url": "/Warehouse/static/css/main.11f41c27.chunk.css"
+  },
+  {
+    "revision": "8100494bc9952abcebcb7ac33f7e4efd",
+    "url": "/Warehouse/index.html"
+  }
+];
